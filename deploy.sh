3569759cc59fbbaa9527f7bb584c07ed09f76eb5
arfan.uddin <arfan.uddin@voiceriver.com>
2021-07-28 21:03:16 -0700

    push docker with sha tag

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@ docker build -t arfanrfn/multi-client:latest -t arfan/multi-client:$SHA -f ./cli
 docker build -t arfanrfn/multi-server:latest -t arfan/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t arfanrfn/multi-worker:latest -t arfan/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push arfanrfn/multi-client:latest
-docker push arfanrfn/multi-server:latest
-docker push arfanrfn/multi-worker:latest
+# docker push arfanrfn/multi-client:latest
+# docker push arfanrfn/multi-server:latest
+# docker push arfanrfn/multi-worker:latest
 
 docker push arfanrfn/multi-client:$SHA
 docker push arfanrfn/multi-server:$SHA
